db9a07b4dc2d2f0addcd5f1c8b756b664cfa6f56
gauteao <99391960+gauteao@users.noreply.github.com>
2024-03-06 09:24:52 +0100

    ye
    ye

--- src/main/java/no/ntnu/Main.java
+++ src/main/java/no/ntnu/Main.java
@@ -1,7 +1,5 @@
 package no.ntnu;
 
-import no.ntnu.MovieTicketClient;
-import no.ntnu.MovieTicketServer;
 public class Main {
     public static void main(String[] args) {
         MovieTicketServer movieTicketServer = new MovieTicketServer("The Matrix", 10);
@@ -13,20 +11,9 @@ public class Main {
         Thread client4 = new Thread(new MovieTicketClient("David", 3, movieTicketServer));
 
         // Starting the threads in order
-        try {
-            client1.start();
-            client1.join(); // Wait for client1 to finish
-
-            client2.start();
-            client2.join(); // Wait for client2 to finish
-
-            client3.start();
-            client3.join(); // Wait for client3 to finish
-
-            client4.start();
-            client4.join(); // Wait for client4 to finish
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        client1.start();
+        client2.start();
+        client3.start();
+        client4.start();
     }
 }
